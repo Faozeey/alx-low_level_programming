8d1c2325a67dc9286a703cbe2e016fe6d88591b1
Faozeey <aboladeakanji18@gmail.com>
2023-03-29 13:35:10 -0700

    C-More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+/*
+* write your line of code here...
+* Note that:
+* - do not use a
+* - do not modify p
+* - you have only one statement
+* - you are not allowed to code anything outside this line of  code
+*/
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
