f16e14651125dce38242f16435ca21d3d9887e97
Faozeey <aboladeakanji18@gmail.com>
2023-04-20 22:21:19 -0700

    C - Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,7 +6,7 @@
 * print_all - prints anything
 * @format: list of types of arguments passed to the function
 */
-	
+
 void print_all(const char * const format, ...)
 {
 	int i = 0;
@@ -40,7 +40,7 @@ void print_all(const char * const format, ...)
 						str = "(nil)";
 				printf("%s%s", sep, str);
 					break;
-			default:
+				default:
 					i++;
 				continue;
 			}
